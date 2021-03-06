9626bc378fff841688ac2c216a2bb2b87c11ff31
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-03-06 17:02:59 +0300

    made non-automatic JSON parsing

--- Superheroes/ViewControllers/MainTableViewController.swift
+++ Superheroes/ViewControllers/MainTableViewController.swift
@@ -69,15 +69,63 @@ extension MainTableViewController {
             case true: stringURL = "https://superheroapi.com/api/3898713710192169/\(id)"
             case false: stringURL = "https://superheroapi.com/api/3898713710192169/\(Int.random(in: rangeAll))"
             }
-
+            
             guard let url = URL(string: stringURL) else { return }
             
             AF.request(url)
-                .responseDecodable(of: Hero.self) { responseData in
+                .validate()
+                .responseJSON { responseData in
                     switch responseData.result {
-                    case .success(let hero):
-                        self.heroes.append(hero)
+                    case .success(let value):
+                        guard let heroData = value as? [String: Any] else { return }
+                        guard let powerstatsHeroData = heroData["powerstats"] as? [String: String] else { return }
+                        guard let biographyHeroData = heroData["biography"] as? [String: Any] else { return }
+                        guard let appearanceHeroData = heroData["appearance"] as? [String: Any] else { return }
+                        guard let workHeroData = heroData["work"] as? [String: String] else { return }
+                        guard let connectionsHeroData = heroData["connections"] as? [String: String] else { return }
+                        guard let imageHeroData = heroData["image"] as? [String: String] else { return }
+                        
+                        let imageHero = ImageHero(url: imageHeroData["url"])
+                        
+                        let connectionsHero = ConnectionsHero(groupAffiliation: connectionsHeroData["group-affiliation"],
+                                                              relatives: connectionsHeroData["relatives"])
+                        
+                        let workHero = WorkHero(occupation: workHeroData["occupation"],
+                                                base: workHeroData["base"])
+                        
+                        let appearanceHero = AppearanceHero(gender: appearanceHeroData["gender"] as? String,
+                                                            race: appearanceHeroData["race"] as? String,
+                                                            height: appearanceHeroData["height"] as? [String],
+                                                            weight: appearanceHeroData["weight"] as? [String],
+                                                            eyeColor: appearanceHeroData["eye-color"] as? String,
+                                                            hairColor: appearanceHeroData["hair-color"] as? String)
+                        
+                        let biographyHero = BiographyHero(fullName: biographyHeroData["full-name"] as? String,
+                                                          alterEgos: biographyHeroData["alter-egos"] as? String,
+                                                          aliases: biographyHeroData["aliases"] as? [String],
+                                                          placeOfBirth: biographyHeroData["place-of-birth"] as? String,
+                                                          firstAppearance: biographyHeroData["first-appearance"] as? String,
+                                                          publisher: biographyHeroData["publisher"] as? String,
+                                                          alignment: biographyHeroData["alignment"] as? String)
+                        
+                        let powerstatsHero = PowerstatsHero(intelligence: powerstatsHeroData["intelligence"],
+                                                            strength: powerstatsHeroData["strength"],
+                                                            speed: powerstatsHeroData["speed"],
+                                                            durability: powerstatsHeroData["durability"],
+                                                            power: powerstatsHeroData["power"],
+                                                            combat: powerstatsHeroData["combat"])
+                        
+                        let hero = Hero(id: heroData["id"] as? String,
+                                        name: heroData["name"] as? String,
+                                        powerstats: powerstatsHero,
+                                        biography: biographyHero,
+                                        appearance: appearanceHero,
+                                        work: workHero,
+                                        connections: connectionsHero,
+                                        image: imageHero)
+                        
                         DispatchQueue.main.async {
+                            self.heroes.append(hero)
                             self.heroes.sort { (hero1, hero0) -> Bool in
                                 if let name1 = hero1.name, let name0 = hero0.name {
                                     return name1 < name0
@@ -86,6 +134,7 @@ extension MainTableViewController {
                             }
                             self.tableView.reloadData()
                         }
+                        
                     case .failure(let error):
                         print(error)
                     }
